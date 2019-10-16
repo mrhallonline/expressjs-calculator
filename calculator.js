d8a7da9b653a16b8511005ae3994842a70d8c03c
Kevin <mrhallonline@gmail.com>
2019-10-15 22:55:56 -0500

    completed BMI calculator in expressjs

--- calculator.js
+++ calculator.js
@@ -5,22 +5,24 @@ const app = express();
 const port = 3000;
 const bodyParser = require("body-parser");
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
 
-app.get("/", function(req, res){
-    res.sendFile(__dirname + "/index.html");
+app.get("/", function (req, res) {
+    res.sendFile(__dirname + "/bmiCalculator.html");
 });
 
-app.post("/", function(req, res){
-    var num1 = Number(req.body.n1);
-    var num2 = Number(req.body.n2);
+app.post("/", function (req, res) {
+    var num1 = Number(req.body.weight);
+    var num2 = Number(req.body.height);
 
-    var result = num1 + num2;
-    
-    res.send("The result of the calculation is " + result);
+    var result = num1 / (num2 * num2);
+
+    res.send("Your BMI is " + result);
 })
 
-app.listen(3000, function(){
+app.listen(3000, function () {
     console.log("server is running on port 3000.");
 });
