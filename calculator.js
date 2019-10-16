3b6c2b9c07fda865595d92e3c0cce457dfeefed7
Kevin <mrhallonline@gmail.com>
2019-10-15 21:00:39 -0500

    completed calculator page using Express.js and Javascript, adds any 2 numbers

--- calculator.js
+++ calculator.js
@@ -3,11 +3,23 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({extended: true}));
+
 
 app.get("/", function(req, res){
-    
-    res.send("<H1>Hello World!</H1>");
+    res.sendFile(__dirname + "/index.html");
 });
+
+app.post("/", function(req, res){
+    var num1 = Number(req.body.n1);
+    var num2 = Number(req.body.n2);
+
+    var result = num1 + num2;
+    
+    res.send("The result of the calculation is " + result);
+})
 
 app.listen(3000, function(){
     console.log("server is running on port 3000.");
