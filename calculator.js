690a5425062d518c86f8d87e3b7dc077ce69b10e
Kevin <mrhallonline@gmail.com>
2019-10-15 20:19:03 -0500

    setup up index page

--- calculator.js
+++ calculator.js
@@ -1,9 +1,14 @@
+//jshint esversion:6
+
 const express = require("express");
 const app = express();
 const port = 3000;
 
 app.get("/", function(req, res){
-    console.log(starting server on port 3000);
-    res.send(<H1>Hello World!</H1>)
+    
+    res.send("<H1>Hello World!</H1>");
+});
 
-})
+app.listen(3000, function(){
+    console.log("server is running on port 3000.");
+});
